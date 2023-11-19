e230cb5873f4518d836727841668acf19f93f8f1
Savodyarkin Nikita <snikitafut@mail.ru>
2023-11-19 18:37:33 +0300

    Array list

---
+++ Example04/Program.cs
@@ -0,0 +1,43 @@
+﻿//метод заполнения массива рандомными числами
+void FillArray(int[] collection) {
+    int length = collection.Length;
+    int index = 0;
+    while(index < length) {
+        collection[index] = new Random().Next(1, 10);
+        index = index + 1;
+    }
+}
+
+//Метод вывода массива
+void PrintArray(int[] col) {
+    int count = col.Length;
+    int position = 0;
+    while(position < count) {
+        Console.Write(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find) {
+    int count = collection.Length;
+    int index = 0;
+    int position = 0;
+    while (index < count) {
+        if(collection[index] == find){
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
