51aeccafeb6f736352620c06b00d607eea4b12a9
Savodyarkin Nikita <snikitafut@mail.ru>
2023-11-19 17:32:45 +0300

    find max value with array

---
+++ Example02/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] array = { 11, 21, 13, 41, 15, 61, 17, 18, 19 };
+
+int Max(int[] array)
+{
+    int result = array.Max();
+    //if (arg2 > result) result = arg2;
+    //if (arg3 > result) result = arg3;
+    return result;
+}
+
+
+
+//int max = Max(
+//        Max(a1, b1, c1),
+//        Max(a2, b2, c2),
+//        Max(a3, b3, c3));
+
+Console.WriteLine(Max(array));
+
